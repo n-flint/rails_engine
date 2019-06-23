23747784dd7984e41efa263ae61198435c73d343
Noah Flint <noah.flint24@gmail.com>
2019-06-23 14:36:36 -0600

    added relationships to item model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,5 +2,10 @@ class Invoice < ApplicationRecord
   validates_presence_of :customer_id,
                         :merchant_id,
                         :status
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
+  has_many :transactions
+  belongs_to :customer
+  belongs_to :merchant
 
 end
