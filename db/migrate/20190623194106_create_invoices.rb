bfcd3c80bce33a3d3ccdea259ab2a1b1aa077247
Noah Flint <noah.flint24@gmail.com>
2019-06-23 13:53:50 -0600

    created migration for items

--- db/migrate/20190623194106_create_invoices.rb
+++ db/migrate/20190623194106_create_invoices.rb
@@ -4,6 +4,7 @@ class CreateInvoices < ActiveRecord::Migration[5.2]
       t.string :status
       t.references :merchant, foreign_key: true
       t.references :customer, foreign_key: true
+      t.timestamps
     end
   end
 end
