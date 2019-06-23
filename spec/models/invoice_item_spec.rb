5fe259eb32ce336d40c254cc6e9e64b0f565144c
Noah Flint <noah.flint24@gmail.com>
2019-06-23 14:38:10 -0600

    added relationship tests to invoice item spec

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
+  describe 'relationships' do
+    it {should belong_to :invoice}
+    it {should belong_to :item}
+  end
   describe 'validations' do
     it {should validate_presence_of :item_id}
     it {should validate_presence_of :invoice_id}
