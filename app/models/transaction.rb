0a4235a743fa8427b731342a0efac59dba42a53a
Noah Flint <noah.flint24@gmail.com>
2019-06-23 14:40:57 -0600

    added relationships to transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,5 @@ class Transaction < ApplicationRecord
                         :credit_card_number,
                         :credit_card_expiration_date,
                         :result
+  belongs_to :invoice
 end
