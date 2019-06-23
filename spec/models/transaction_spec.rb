6ec12a4980445b2cbcefb3ac50d60fb448efa1c1
Noah Flint <noah.flint24@gmail.com>
2019-06-23 14:21:17 -0600

    added relationship tests to customer spec

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -5,6 +5,6 @@ RSpec.describe Transaction, type: :model do
     it {should validate_presence_of :invoice_id}
     it {should validate_presence_of :credit_card_number}
     it {should validate_presence_of :credit_card_expiration_date}
-    it {should validate_presence_of :result}=
+    it {should validate_presence_of :result}
   end
 end
