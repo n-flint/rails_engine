ee0335923cf1f827f43c364ac99f7c4cfb6c0fb3
Noah Flint <noah.flint24@gmail.com>
2019-06-24 16:03:51 -0600

    test for search multiple merchants by updated_at

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -80,10 +80,10 @@ RSpec.describe 'Merchants API' do
 
   context 'finds multiple merchants' do
     before :each do
-      @merchant1 = Merchant.create(id: 1, name: 'Cummings-Thiel')
+      @merchant1 = Merchant.create(id: 1, name: 'Cummings-Thiel', created_at: '2019-06-23 14:53:59 UTC')
       @merchant2= Merchant.create(name: 'Noah', created_at: '2019-06-23 14:53:59 UTC')
       @merchant3 = Merchant.create(name: 'Cummings-Thiel', updated_at: '2019-04-24 12:53:59 UTC')
-      @merchant4 = Merchant.create(name: 'Schroeder-Jerde')
+      @merchant4 = Merchant.create(name: 'Schroeder-Jerde', updated_at: '2019-04-24 12:53:59 UTC')
     end
 
     it 'sends a list of merchants' do
@@ -110,7 +110,7 @@ RSpec.describe 'Merchants API' do
       expect(data['data'][1]['attributes']['id']).to eq(@merchant3.id)
     end
 
-    it 'can find multiple merchants by name' do
+    it 'can find multiple merchants by id' do
       get '/api/v1/merchants/find_all?id=1'
 
       expect(response).to be_successful
@@ -129,9 +129,25 @@ RSpec.describe 'Merchants API' do
 
       data = JSON.parse(response.body)
 
-      expect(data['data'].count).to eq(1)
-      expect(data['data'][0]['attributes']['name']).to eq(@merchant2.name)
-      expect(data['data'][0]['attributes']['id']).to eq(@merchant2.id)
+      expect(data['data'].count).to eq(2)
+      expect(data['data'][0]['attributes']['name']).to eq(@merchant1.name)
+      expect(data['data'][0]['attributes']['id']).to eq(@merchant1.id)
+      expect(data['data'][1]['attributes']['name']).to eq(@merchant2.name)
+      expect(data['data'][1]['attributes']['id']).to eq(@merchant2.id)
+    end
+
+    it 'can find multiple merchants by updated_at' do
+      get '/api/v1/merchants/find_all?updated_at=2019-04-24-12:53:59-UTC'
+
+      expect(response).to be_successful
+
+      data = JSON.parse(response.body)
+
+      expect(data['data'].count).to eq(2)
+      expect(data['data'][0]['attributes']['name']).to eq(@merchant3.name)
+      expect(data['data'][0]['attributes']['id']).to eq(@merchant3.id)
+      expect(data['data'][1]['attributes']['name']).to eq(@merchant4.name)
+      expect(data['data'][1]['attributes']['id']).to eq(@merchant4.id)
     end
   end
 end
