7bb7218e06bac5cd6a0f30788c88d05f7324cef0
Noah Flint <noah.flint24@gmail.com>
2019-06-24 17:40:07 -0600

    added  /:id/items route to merchants namespace

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
         get '/random', to: 'random#show'
+        get '/:id/items', to: 'items#index'
       end
       resources :merchants, only: [:index, :show]
     end
