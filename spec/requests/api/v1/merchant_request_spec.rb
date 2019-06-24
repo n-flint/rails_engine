6f3f7ceb0002138a587c7873f6f43c9d9478451c
Noah Flint <noah.flint24@gmail.com>
2019-06-24 08:27:32 -0600

    created merchant request spec

---
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchants API' do
+  it 'sends a list of merchants' do
+    merchant1 = Merchant.create(name: 'Trevor')
+    merchant2= Merchant.create(name: 'Noah')
+    merchant3 = Merchant.create(name: 'Earl')
+    get '/api/v1/merchants' 
+
+    expect(response).to be_successful
+
+    data = JSON.parse(response.body)
+
+    expect(data.count).to eq(3)
+  end
+end
