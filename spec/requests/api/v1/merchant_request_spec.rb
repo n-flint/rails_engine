4794b84b9ebdfd95bef4bd7093033021e340ecea
Noah Flint <noah.flint24@gmail.com>
2019-06-24 15:05:41 -0600

    test for searching multiple merchants by name

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchants API' do
+  context 'finds a single merchant' do
+
   before :each do
     @merchant1 = Merchant.create(id: 1, name: 'Trevor')
     @merchant2= Merchant.create(name: 'Noah', created_at: '2019-06-23 14:53:59 UTC')
@@ -8,69 +10,107 @@ RSpec.describe 'Merchants API' do
     @merchant4 = Merchant.create(name: 'Schroeder-Jerde')
   end
 
-  it 'sends a list of merchants' do
-    get '/api/v1/merchants.json' 
+    it 'sends a list of merchants' do
+      get '/api/v1/merchants.json' 
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    data = JSON.parse(response.body)
+      data = JSON.parse(response.body)
 
-    expect(data['data'].count).to eq(4)
+      expect(data['data'].count).to eq(4)
+    end
+
+    it 'sends a single merchant' do
+      get '/api/v1/merchants/1.json'
+
+      expect(response).to be_successful
+
+      data = JSON.parse(response.body)
+
+
+      expect(data['data']['attributes'].count).to eq(2)
+      expect(data['data']['attributes']['id']).to eq(1)
+      expect(data['data']['attributes']['name']).to eq(@merchant1.name)
+    end
+
+    it 'finds a single merchant by name' do
+      get '/api/v1/merchants/find?name=Schroeder-Jerde'
+
+      expect(response).to be_successful
+
+      data = JSON.parse(response.body)
+
+      expect(data['data'][0]['attributes']['name']).to eq(@merchant4.name)
+      expect(data['data'][0]['attributes']['id']).to eq(@merchant4.id)
+    end
+
+    it 'finds a single merchant by id' do
+      get '/api/v1/merchants/find?id=1'
+
+      expect(response).to be_successful
+
+      data = JSON.parse(response.body)
+
+      expect(data['data'][0]['attributes']['name']).to eq(@merchant1.name)
+      expect(data['data'][0]['attributes']['id']).to eq(@merchant1.id)
+    end
+
+    it 'finds a single merchant by created_at' do
+      get '/api/v1/merchants/find?created_at=2019-06-23-14:53:59-UTC'
+
+      expect(response).to be_successful
+
+      data = JSON.parse(response.body)
+
+      expect(data['data'][0]['attributes']['name']).to eq(@merchant2.name)
+      expect(data['data'][0]['attributes']['id']).to eq(@merchant2.id)
+    end
+
+    it 'finds a single merchant by updated_at' do
+      get '/api/v1/merchants/find?updated_at=2019-04-24-12:53:59-UTC'
+
+      expect(response).to be_successful
+
+      data = JSON.parse(response.body)
+
+      expect(data['data'][0]['attributes']['name']).to eq(@merchant3.name)
+      expect(data['data'][0]['attributes']['id']).to eq(@merchant3.id)
+    end
   end
 
-  it 'sends a single merchant' do
-    get '/api/v1/merchants/1.json'
+  context 'finds multiple merchants' do
+    before :each do
+      @merchant1 = Merchant.create(id: 1, name: 'Cummings-Thiel')
+      @merchant2= Merchant.create(name: 'Noah', created_at: '2019-06-23 14:53:59 UTC')
+      @merchant3 = Merchant.create(name: 'Cummings-Thiel', updated_at: '2019-04-24 12:53:59 UTC')
+      @merchant4 = Merchant.create(name: 'Schroeder-Jerde')
+    end
 
-    expect(response).to be_successful
+    it 'sends a list of merchants' do
+      get '/api/v1/merchants.json' 
 
-    data = JSON.parse(response.body)
+      expect(response).to be_successful
 
-    expect(data['data']['attributes'].count).to eq(2)
-    expect(data['data']['attributes']['id']).to eq(1)
-    expect(data['data']['attributes']['name']).to eq(@merchant1.name)
+      data = JSON.parse(response.body)
+
+      expect(data['data'].count).to eq(4)
+    end
+
+    it 'can find multiple merchants by name' do
+      get '/api/v1/merchants/find_all?name=Cummings-Thiel'
+
+      expect(response).to be_successful
+
+      data = JSON.parse(response.body)
+
+      expect(data['data'].count).to eq(2)
+      expect(data['data'][0]['attributes']['name']).to eq(@merchant1.name)
+      expect(data['data'][0]['attributes']['id']).to eq(@merchant1.id)
+      expect(data['data'][1]['attributes']['name']).to eq(@merchant3.name)
+      expect(data['data'][1]['attributes']['id']).to eq(@merchant3.id)
+    end
+
   end
 
-  it 'finds a single merchant by name' do
-    get '/api/v1/merchants/find?name=Schroeder-Jerde'
-
-    expect(response).to be_successful
-
-    data = JSON.parse(response.body)
-
-    expect(data['data'][0]['attributes']['name']).to eq(@merchant4.name)
-
-  end
-
-  it 'finds a single merchant by id' do
-    get '/api/v1/merchants/find?id=1'
-
-    expect(response).to be_successful
-
-    data = JSON.parse(response.body)
-
-    expect(data['data'][0]['attributes']['name']).to eq(@merchant1.name)
-    expect(data['data'][0]['attributes']['id']).to eq(@merchant1.id)
-  end
-
-  it 'finds a single merchant by created_at' do
-    get '/api/v1/merchants/find?created_at=2019-06-23-14:53:59-UTC'
-
-    expect(response).to be_successful
-
-    data = JSON.parse(response.body)
-
-    expect(data['data'][0]['attributes']['name']).to eq(@merchant2.name)
-    expect(data['data'][0]['attributes']['id']).to eq(@merchant2.id)
-  end
-
-  it 'finds a single merchant by updated_at' do
-    get '/api/v1/merchants/find?updated_at=2019-04-24-12:53:59-UTC'
-
-    expect(response).to be_successful
-
-    data = JSON.parse(response.body)
-
-    expect(data['data'][0]['attributes']['name']).to eq(@merchant3.name)
-    expect(data['data'][0]['attributes']['id']).to eq(@merchant3.id)
-  end
+  
 end
