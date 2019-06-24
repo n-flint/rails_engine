879b6c9199599cb7f634ff24a8a49a51bd9630c5
Noah Flint <noah.flint24@gmail.com>
2019-06-24 14:24:28 -0600

    refactored nested expectation

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Merchants API' do
     @merchant1 = Merchant.create(id: 1, name: 'Trevor')
     @merchant2= Merchant.create(name: 'Noah')
     @merchant3 = Merchant.create(name: 'Earl')
+    @merchant4 = Merchant.create(name: 'Schroeder-Jerde')
   end
 
   it 'sends a list of merchants' do
@@ -14,7 +15,7 @@ RSpec.describe 'Merchants API' do
 
     data = JSON.parse(response.body)
 
-    expect(data['data'].count).to eq(3)
+    expect(data['data'].count).to eq(4)
   end
 
   it 'sends a single merchant' do
@@ -26,6 +27,16 @@ RSpec.describe 'Merchants API' do
 
     expect(data['data']['attributes'].count).to eq(2)
     expect(data['data']['attributes']['id']).to eq(1)
-    expect(data['data']['attributes']['name']).to eq('Trevor')
+    expect(data['data']['attributes']['name']).to eq(@merchant1.name)
+  end
+
+  it 'finds a single merchant by name' do
+    get '/api/v1/merchants/find?name=Schroeder-Jerde'
+
+    expect(response).to be_successful
+
+    data = JSON.parse(response.body)
+
+    expect(data['data'][0]['attributes']['name']).to eq(@merchant4.name)
   end
 end
