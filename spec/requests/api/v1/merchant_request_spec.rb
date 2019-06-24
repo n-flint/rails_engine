5acf1a132c888161c5bb519ba5267dcbcbbaa7c1
Noah Flint <noah.flint24@gmail.com>
2019-06-24 10:12:53 -0600

    created test for single merchant

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Merchants API' do
   before :each do
-    @merchant1 = Merchant.create(name: 'Trevor')
+    @merchant1 = Merchant.create(id: 1, name: 'Trevor')
     @merchant2= Merchant.create(name: 'Noah')
     @merchant3 = Merchant.create(name: 'Earl')
   end
 
   it 'sends a list of merchants' do
-    get '/api/v1/merchants' 
+    get '/api/v1/merchants.json' 
 
     expect(response).to be_successful
 
@@ -20,12 +20,12 @@ RSpec.describe 'Merchants API' do
   it 'sends a single merchant' do
     get '/api/v1/merchants/1.json'
 
-    expect(response).to be successful
+    expect(response).to be_successful
 
     data = JSON.parse(response.body)
 
-    expect(data['data'].count).to eq(1)
-    # data['data'].first['attributes'].count
     expect(data['data']['attributes'].count).to eq(2)
+    expect(data['data']['attributes']['id']).to eq(1)
+    expect(data['data']['attributes']['name']).to eq('Trevor')
   end
 end
