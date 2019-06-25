61c0e75b1ff50374a5818ec89b97f5e94a17060b
Noah Flint <noah.flint24@gmail.com>
2019-06-25 12:50:43 -0600

    created test for multiple transaction request

---
+++ spec/requests/api/v1/transaction/multiple_transaction_request_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Transactions Api' do
+  context 'finds multiple transactions' do
+    before :each do
+      @customer1 = Customer.create(first_name: 'Bill', last_name: 'Burr')
+      @merchant1 = Merchant.create(name: 'Afroman') 
+
+      @invoice1 = Invoice.create(status: 'shipped', customer: @customer1, merchant: @merchant1)
+      @invoice2 = Invoice.create(status: 'shipped', customer: @customer1, merchant: @merchant1)
+      @invoice3 = Invoice.create(status: 'shipped', customer: @customer1, merchant: @merchant1)
+      @invoice4 = Invoice.create(status: 'shipped', customer: @customer1, merchant: @merchant1)
+
+      @transaction1 = Transaction.create(invoice: @invoice1, credit_card_number: '12341234123412334', credit_card_expiration_date: '2019-04-12', result: 'success')
+      @transaction2 = Transaction.create(invoice: @invoice2, credit_card_number: '00000000000000000', credit_card_expiration_date: '2019-04-11', result: 'success')
+      @transaction3 = Transaction.create(invoice: @invoice3, credit_card_number: '1111111111111111', credit_card_expiration_date: '2019-04-10', result: 'success')
+      @transaction4 = Transaction.create(invoice: @invoice4, credit_card_number: '2222222222222222', credit_card_expiration_date: '2019-04-09', result: 'failed')
+    end
+    
+    it 'sends a list of transactions' do
+      get '/api/v1/transactions'
+
+      expect(response).to be_successful
+
+      data = JSON.parse(response.body)
+
+      expect(data['data'].count).to eq(4)
+    end
+  end
+end
