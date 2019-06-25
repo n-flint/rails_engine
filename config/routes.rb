b511344bb3c1d4916a8ebc414a26e0b61b8b741f
Noah Flint <noah.flint24@gmail.com>
2019-06-24 18:11:55 -0600

    added route for /:id/invoices for merchant

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
         get '/find_all', to: 'search#index'
         get '/random', to: 'random#show'
         get '/:id/items', to: 'items#index'
+        get '/:id/invoices', to: 'invoices#index'
       end
       resources :merchants, only: [:index, :show]
     end
