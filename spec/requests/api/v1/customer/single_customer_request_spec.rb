bfa1914e009da1b1c9e4365d4be091906494418d
Noah Flint <noah.flint24@gmail.com>
2019-06-24 21:14:45 -0600

    refactored expectaion

--- spec/requests/api/v1/customer/single_customer_request_spec.rb
+++ spec/requests/api/v1/customer/single_customer_request_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe 'Customers API' do
   context 'finds a single customer' do
     before :each do
-      @customer1 = Customer.create(id: 1, first_name: 'bob', last_name: 'kelly')
+      @customer1 = Customer.create(first_name: 'Bobby', last_name: 'Kelly')
+      @customer2 = Customer.create(first_name: 'Bert', last_name: 'Kreischer')
     end
     it 'sends a single customer' do
-      get 'api/v1/customers/1/.json'
+      get "/api/v1/customers/#{@customer1.id}.json"
 
       data = JSON.parse(response.body)
 
-      expect(data['data'].count).to eq(1)
-      expect(data['data']['attributes']['id']).to eq(@customer.id)
+      expect(data['data']['attributes']['id']).to eq(@customer1.id)
       expect(data['data']['attributes']['first_name']).to eq(@customer1.first_name)
       expect(data['data']['attributes']['last_name']).to eq(@customer1.last_name)
     end
