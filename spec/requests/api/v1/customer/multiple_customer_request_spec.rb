3f9c633ca6c1e7dfde16fa35a053c1d9a0be0126
Noah Flint <noah.flint24@gmail.com>
2019-06-24 22:52:19 -0600

    added test to find multiple customers by first name

--- spec/requests/api/v1/customer/multiple_customer_request_spec.rb
+++ spec/requests/api/v1/customer/multiple_customer_request_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe 'Customers API' do
       @customer1 = Customer.create(first_name: 'Bobby', last_name: 'Kelly')
       @customer2 = Customer.create(first_name: 'Bert', last_name: 'Kreischer')
       @customer3 = Customer.create(first_name: 'Bill', last_name: 'Bull', created_at: '2019-06-23 14:53:59 UTC')
-      @customer4 = Customer.create(first_name: 'Bill', last_name: 'Bull', updated_at: '2019-06-25 14:53:59 UTC')
+      @customer4 = Customer.create(first_name: 'Bill', last_name: 'Burr', updated_at: '2019-06-25 14:53:59 UTC')
+      @customer5 = Customer.create(first_name: 'Bobby', last_name: 'Burr', updated_at: '2019-06-25 14:53:59 UTC')
     end
     
     it 'sends a list of customers' do
@@ -16,7 +17,7 @@ RSpec.describe 'Customers API' do
 
       data = JSON.parse(response.body)
 
-      expect(data['data'].count).to eq(4)
+      expect(data['data'].count).to eq(5)
     end
 
     it 'can find multiple customers by id' do
@@ -25,12 +26,27 @@ RSpec.describe 'Customers API' do
       expect(response).to be_successful
 
       data = JSON.parse(response.body)
-      require 'pry'; binding.pry
 
       expect(data['data'].count).to eq(1)
       expect(data['data'][0]['attributes']['id']).to eq(@customer1.id)
       expect(data['data'][0]['attributes']['first_name']).to eq(@customer1.first_name)
       expect(data['data'][0]['attributes']['last_name']).to eq(@customer1.last_name)
+    end
+
+    it 'can find multiple customers by first name' do
+      get '/api/v1/customers/find_all?first_name=Bill'
+
+      expect(response).to be_successful
+
+      data = JSON.parse(response.body)
+
+      expect(data['data'].count).to eq(2)
+      expect(data['data'][0]['attributes']['id']).to eq(@customer3.id)
+      expect(data['data'][0]['attributes']['first_name']).to eq(@customer3.first_name)
+      expect(data['data'][0]['attributes']['last_name']).to eq(@customer3.last_name)
+      expect(data['data'][1]['attributes']['id']).to eq(@customer4.id)
+      expect(data['data'][1]['attributes']['first_name']).to eq(@customer4.first_name)
+      expect(data['data'][1]['attributes']['last_name']).to eq(@customer4.last_name)
     end
   end
 end
