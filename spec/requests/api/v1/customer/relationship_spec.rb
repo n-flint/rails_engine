3b4e82d227a73e6e5462a64e77678cfaf0e9002f
Noah Flint <noah.flint24@gmail.com>
2019-06-25 10:29:48 -0600

    refactored expectation format for transaction.credit_card_expiration_date

--- spec/requests/api/v1/customer/relationship_spec.rb
+++ spec/requests/api/v1/customer/relationship_spec.rb
@@ -15,9 +15,9 @@ RSpec.describe 'Customers API' do
       @item2 = Item.create(name: 'item 2', description:'item 2 description', unit_price: '20000')
       @merchant1.items << [@item1, @item2]
 
-      @transaction1 = Transaction.create(invoice: @invoice1, credit_card_number: '12341234123412334', credit_card_expiration_date: '12-05-19', result: 'success')
+      @transaction1 = Transaction.create(invoice: @invoice1, credit_card_number: '12341234123412334', credit_card_expiration_date: '2019-05-12', result: 'success')
 
-      @transaction2 = Transaction.create(invoice: @invoice2, credit_card_number: '12341234123412334', credit_card_expiration_date: '12-05-19', result: 'success')
+      @transaction2 = Transaction.create(invoice: @invoice2, credit_card_number: '12341234123412334', credit_card_expiration_date: '2019-05-12', result: 'success')
     end
 
     it 'can return a customers invoices' do
@@ -44,13 +44,13 @@ RSpec.describe 'Customers API' do
       expect(data['data'].count).to eq(2)
       expect(data['data'][0]['id'].to_i).to eq(@transaction1.id)
       expect(data['data'][0]['attributes']['credit_card_number']).to eq(@transaction1.credit_card_number)
-      expect(data['data'][0]['attributes']['credit_card_expiration_date']).to eq(@transaction1.credit_card_expiration_date)
-      expect(data['data'][0]['attributes']['status']).to eq(@transaction1.status)
+      expect(data['data'][0]['attributes']['credit_card_expiration_date']).to eq('2019-05-12')
+      expect(data['data'][0]['attributes']['result']).to eq(@transaction1.result)
 
       expect(data['data'][1]['id'].to_i).to eq(@transaction2.id)
-      expect(data['data'][1]['attributes']['credit_card_number']).to eq(@invoice2.credit_card_number)
-      expect(data['data'][1]['attributes']['credit_card_expiration_date']).to eq(@transaction2.credit_card_expiration_date)
-      expect(data['data'][1]['attributes']['status']).to eq(@transaction2.status)
+      expect(data['data'][1]['attributes']['credit_card_number']).to eq(@transaction2.credit_card_number)
+      expect(data['data'][1]['attributes']['credit_card_expiration_date']).to eq('2019-05-12')
+      expect(data['data'][1]['attributes']['result']).to eq(@transaction2.result)
     end
   end
 end
