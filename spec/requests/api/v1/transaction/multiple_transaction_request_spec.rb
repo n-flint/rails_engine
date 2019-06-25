903a5b439ebed16e5da7475bc7d539867c0db92f
Noah Flint <noah.flint24@gmail.com>
2019-06-25 14:11:44 -0600

    test for multiple transactions by result

--- spec/requests/api/v1/transaction/multiple_transaction_request_spec.rb
+++ spec/requests/api/v1/transaction/multiple_transaction_request_spec.rb
@@ -11,10 +11,10 @@ RSpec.describe 'Transactions Api' do
       @invoice3 = Invoice.create(status: 'shipped', customer: @customer1, merchant: @merchant1)
       @invoice4 = Invoice.create(status: 'shipped', customer: @customer1, merchant: @merchant1)
 
-      @transaction1 = Transaction.create(invoice: @invoice1, credit_card_number: '12341234123412334', credit_card_expiration_date: '2019-04-12', result: 'success')
-      @transaction2 = Transaction.create(invoice: @invoice2, credit_card_number: '00000000000000000', credit_card_expiration_date: '2019-04-11', result: 'success')
-      @transaction3 = Transaction.create(invoice: @invoice3, credit_card_number: '1111111111111111', credit_card_expiration_date: '2019-04-10', result: 'success')
-      @transaction4 = Transaction.create(invoice: @invoice4, credit_card_number: '2222222222222222', credit_card_expiration_date: '2019-04-09', result: 'failed')
+      @transaction1 = Transaction.create(invoice: @invoice1, credit_card_number: '1111111111111111', credit_card_expiration_date: '2019-04-12', result: 'success')
+      @transaction2 = Transaction.create(invoice: @invoice2, credit_card_number: '00000000000000000', credit_card_expiration_date: '2019-04-11', result: 'failed')
+      @transaction3 = Transaction.create(invoice: @invoice3, credit_card_number: '1111111111111111', credit_card_expiration_date: '2019-04-10', result: 'failed')
+      @transaction4 = Transaction.create(invoice: @invoice4, credit_card_number: '2222222222222222', credit_card_expiration_date: '2019-04-10', result: 'success')
     end
     
     it 'sends a list of transactions' do
@@ -46,9 +46,13 @@ RSpec.describe 'Transactions Api' do
 
       data = JSON.parse(response.body)
 
-      expect(data['data'][0]['attributes']['id']).to eq(@transaction3.id)
-      expect(data['data'][0]['attributes']['credit_card_number']).to eq(@transaction3.credit_card_number)
-      expect(data['data'][0]['attributes']['credit_card_expiration_date']).to eq('2019-04-10')
+      expect(data['data'][0]['attributes']['id']).to eq(@transaction1.id)
+      expect(data['data'][0]['attributes']['credit_card_number']).to eq(@transaction1.credit_card_number)
+      expect(data['data'][0]['attributes']['credit_card_expiration_date']).to eq('2019-04-12')
+
+      expect(data['data'][1]['attributes']['id']).to eq(@transaction3.id)
+      expect(data['data'][1]['attributes']['credit_card_number']).to eq(@transaction3.credit_card_number)
+      expect(data['data'][1]['attributes']['credit_card_expiration_date']).to eq('2019-04-10')
     end
 
     it 'can find multiple transactions by credit card number' do
@@ -58,9 +62,29 @@ RSpec.describe 'Transactions Api' do
 
       data = JSON.parse(response.body)
 
-      expect(data['data'][0]['attributes']['id']).to eq(@transaction4.id)
-      expect(data['data'][0]['attributes']['credit_card_number']).to eq(@transaction4.credit_card_number)
-      expect(data['data'][0]['attributes']['credit_card_expiration_date']).to eq('2019-04-09')
+      expect(data['data'][0]['attributes']['id']).to eq(@transaction3.id)
+      expect(data['data'][0]['attributes']['credit_card_number']).to eq(@transaction3.credit_card_number)
+      expect(data['data'][0]['attributes']['credit_card_expiration_date']).to eq('2019-04-10')
+
+      expect(data['data'][1]['attributes']['id']).to eq(@transaction4.id)
+      expect(data['data'][1]['attributes']['credit_card_number']).to eq(@transaction4.credit_card_number)
+      expect(data['data'][1]['attributes']['credit_card_expiration_date']).to eq('2019-04-10')
+    end
+
+    it 'can find multiple transactions by status' do
+      get '/api/v1/transactions/find_all?result=failed'
+
+      expect(response).to be_successful
+
+      data = JSON.parse(response.body)
+
+      expect(data['data'][0]['attributes']['id']).to eq(@transaction2.id)
+      expect(data['data'][0]['attributes']['credit_card_number']).to eq(@transaction2.credit_card_number)
+      expect(data['data'][0]['attributes']['credit_card_expiration_date']).to eq('2019-04-11')
+
+      expect(data['data'][1]['attributes']['id']).to eq(@transaction3.id)
+      expect(data['data'][1]['attributes']['credit_card_number']).to eq(@transaction3.credit_card_number)
+      expect(data['data'][1]['attributes']['credit_card_expiration_date']).to eq('2019-04-10')
     end
   end
 end
