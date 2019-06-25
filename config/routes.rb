1bf5f2c897521a4e76f2648d7c94f349c0b96f10
Noah Flint <noah.flint24@gmail.com>
2019-06-24 22:43:18 -0600

    added customer resources index

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
       
       namespace :customers do
         get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
       end
       
       resources :customers, only: [:index, :show]
