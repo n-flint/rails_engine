7d8822a49022316f94b8f3829be1e43492d3603d
Noah Flint <noah.flint24@gmail.com>
2019-06-24 23:00:01 -0600

    test for finding multiple customers by created_at

--- spec/requests/api/v1/customer/multiple_customer_request_spec.rb
+++ spec/requests/api/v1/customer/multiple_customer_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Customers API' do
   context 'finds multiple customers' do
     before :each do
-      @customer1 = Customer.create(first_name: 'Bobby', last_name: 'Kelly')
+      @customer1 = Customer.create(first_name: 'Bobby', last_name: 'Kelly', created_at: '2019-06-23 14:53:59 UTC')
       @customer2 = Customer.create(first_name: 'Bert', last_name: 'Kreischer')
       @customer3 = Customer.create(first_name: 'Bill', last_name: 'Bull', created_at: '2019-06-23 14:53:59 UTC')
       @customer4 = Customer.create(first_name: 'Bill', last_name: 'Burr', updated_at: '2019-06-25 14:53:59 UTC')
@@ -63,6 +63,22 @@ RSpec.describe 'Customers API' do
       expect(data['data'][1]['attributes']['id']).to eq(@customer5.id)
       expect(data['data'][1]['attributes']['first_name']).to eq(@customer5.first_name)
       expect(data['data'][1]['attributes']['last_name']).to eq(@customer5.last_name)
+    end
+
+    it 'can find multiple customers by created_at' do
+      get '/api/v1/customers/find_all?created_at=2019-06-23-14:53:59-UTC'
+
+      expect(response).to be_successful
+
+      data = JSON.parse(response.body)
+
+      expect(data['data'].count).to eq(2)
+      expect(data['data'][0]['attributes']['id']).to eq(@customer1.id)
+      expect(data['data'][0]['attributes']['first_name']).to eq(@customer1.first_name)
+      expect(data['data'][0]['attributes']['last_name']).to eq(@customer1.last_name)
+      expect(data['data'][1]['attributes']['id']).to eq(@customer3.id)
+      expect(data['data'][1]['attributes']['first_name']).to eq(@customer3.first_name)
+      expect(data['data'][1]['attributes']['last_name']).to eq(@customer3.last_name)
     end
   end
 end
