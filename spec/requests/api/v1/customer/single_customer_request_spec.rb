48c768d1a00d886567df2e4a4509f71f3e0006bc
Noah Flint <noah.flint24@gmail.com>
2019-06-24 22:12:19 -0600

    test for finding customer by created_at

--- spec/requests/api/v1/customer/single_customer_request_spec.rb
+++ spec/requests/api/v1/customer/single_customer_request_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Customers API' do
     before :each do
       @customer1 = Customer.create(first_name: 'Bobby', last_name: 'Kelly')
       @customer2 = Customer.create(first_name: 'Bert', last_name: 'Kreischer')
-      @customer3 = Customer.create(first_name: 'Bill', last_name: 'Bull')
+      @customer3 = Customer.create(first_name: 'Bill', last_name: 'Bull', created_at: '2019-06-23 14:53:59 UTC')
     end
     it 'sends a single customer' do
       get "/api/v1/customers/#{@customer1.id}.json"
@@ -39,6 +39,16 @@ RSpec.describe 'Customers API' do
 
     it 'finds a single customer by last name' do
       get "/api/v1/customers/find/?last_name=#{@customer3.last_name}"
+
+      data = JSON.parse(response.body)
+
+      expect(data['data'][0]['attributes']['id']).to eq(@customer3.id)
+      expect(data['data'][0]['attributes']['first_name']).to eq(@customer3.first_name)
+      expect(data['data'][0]['attributes']['last_name']).to eq(@customer3.last_name)
+    end
+
+    it 'finds a single customer by created_at' do
+      get "/api/v1/customers/find/?created_at=#{@customer3.created_at}"
 
       data = JSON.parse(response.body)
 
