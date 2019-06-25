c72a9ffcb7e1167a06787e4dcc660dd5a67a5266
Noah Flint <noah.flint24@gmail.com>
2019-06-25 17:02:19 -0600

    added random method to show action

--- app/controllers/api/v1/merchants/random_controller.rb
+++ app/controllers/api/v1/merchants/random_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::Merchants::RandomController < ApplicationController
 
   def show
-    render json: MerchantSerializer.new(Merchant.all.sample)
+    # require 'pry'; binding.pry
+    # render json: MerchantSerializer.new(Merchant.all.sample)
+    render json: MerchantSerializer.new(Merchant.random)
+    # test = {json: MerchantSerializer.new(Merchant.first)}
+    # render json: MerchantSerializer.new(Merchant.first)
+    # require 'pry'; binding.pry
+
   end
 
 end
