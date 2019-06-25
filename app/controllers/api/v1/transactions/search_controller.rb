7cb19aa73368cc2baef4ada72595a787af2bfb38
Noah Flint <noah.flint24@gmail.com>
2019-06-25 14:00:00 -0600

    removed white space

--- app/controllers/api/v1/transactions/search_controller.rb
+++ app/controllers/api/v1/transactions/search_controller.rb
@@ -8,7 +8,6 @@ class Api::V1::Transactions::SearchController < ApplicationController
     render json: TransactionSerializer.new(Transaction.where(transaction_params))
   end
   
-
   private
     def transaction_params
       params.permit(:id, :credit_card_number, :credit_card_expiration_date, :result)
