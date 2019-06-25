cd172f23ea49d305271f43e6ed3a718736c550e8
Noah Flint <noah.flint24@gmail.com>
2019-06-25 12:24:25 -0600

    created search controller with show action and strong params

---
+++ app/controllers/api/v1/transactions/search_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::Transactions::SearchController < ApplicationController
+
+  def show
+    render json: TransactionSerializer.new(Transaction.where(transaction_params))
+  end
+
+  private
+    def transaction_params
+      params.permit(:id, :credit_card_number, :credit_card_expiration_date, :result)
+    end
+end
