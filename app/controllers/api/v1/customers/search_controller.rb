94e47a0a50382c069fc41c3c6f5a1d82be4d0696
Noah Flint <noah.flint24@gmail.com>
2019-06-24 22:43:34 -0600

    added index action to search controller

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -4,6 +4,10 @@ class Api::V1::Customers::SearchController < ApplicationController
     render json: CustomerSerializer.new(Customer.where(customer_params))
   end
 
+  def index
+    render json: CustomerSerializer.new(Customer.where(customer_params))
+  end
+
   private
     def customer_params
       params.permit(:id, :first_name, :last_name, :created_at, :updated_at)
