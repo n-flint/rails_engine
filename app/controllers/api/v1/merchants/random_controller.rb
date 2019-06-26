5760a1a03e2341d6544d1332f2776995ef5bb611
Noah Flint <noah.flint24@gmail.com>
2019-06-26 11:45:45 -0600

    added Merchant.random to random controller

--- app/controllers/api/v1/merchants/random_controller.rb
+++ app/controllers/api/v1/merchants/random_controller.rb
@@ -1,13 +1,6 @@
 class Api::V1::Merchants::RandomController < ApplicationController
 
   def show
-    # require 'pry'; binding.pry
-    # render json: MerchantSerializer.new(Merchant.all.sample)
     render json: MerchantSerializer.new(Merchant.random)
-    # test = {json: MerchantSerializer.new(Merchant.first)}
-    # render json: MerchantSerializer.new(Merchant.first)
-    # require 'pry'; binding.pry
-
   end
-
 end
