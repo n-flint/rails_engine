9b8e765fd41bec0eecad0c026eacaed9d7c3008e
Noah Flint <noah.flint24@gmail.com>
2019-06-26 21:47:59 -0600

    created test for single item request

---
+++ spec/requests/api/v1/item/single_item_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'Items API' do
+  context 'single item' do
+    before :each do
+      @merchant1 = Merchant.create(name: 'Trevor')
+      @merchant2 = Merchant.create(name: 'Bill')
+      @item1 = Item.create(name: 'item 1', description: 'item 1 description', unit_price: 10000, merchant: @merchant1)
+      @item2 = Item.create(name: 'item 2', description: 'item 2 description', unit_price: 20000, merchant: @merchant2)
+
+    end
+    it 'sends a single item' do
+      get "/api/v1/items/#{@item.id}.json"
+
+      expect(response).to be_successful
+
+      data = JSON.parse(response.body)
+
+      expect(data['data']['attributes']['id']).to eq(@item.id)
+      expect(data['data']['attributes']['name']).to eq(@item.name)
+      expect(data['data']['attributes']['description']).to eq(@item.description)
+      expect(data['data']['attributes']['description']).to eq(@item.description)
+      expect(data['data']['attributes']['unit_price']).to eq(@item.unit_price)
+    end
+  end
+end
