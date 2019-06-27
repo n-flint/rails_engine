76ad3b85e0503a919983b4cc1f5df35b4050ac3e
Noah Flint <noah.flint24@gmail.com>
2019-06-27 14:38:46 -0600

    created merchants/revenue route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
         get '/:id/invoices', to: 'invoices#index'
         get '/most_revenue', to: 'most_revenue#index'
         get '/most_items', to: 'most_items#index'
+        get '/revenue', to: 'date_revenue#show'
       end
       resources :merchants, only: [:index, :show]
 
