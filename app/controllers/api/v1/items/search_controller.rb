effec373063e0ea79790898b7ba22fee00e4dd66
Noah Flint <noah.flint24@gmail.com>
2019-06-26 22:51:04 -0600

    added unit price and desctsiption to item_params

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -6,6 +6,6 @@ class Api::V1::Items::SearchController < ApplicationController
 
   private
     def item_params
-      params.permit(:id, :name)
+      params.permit(:id, :name, :description, :unit_price)
     end
 end
