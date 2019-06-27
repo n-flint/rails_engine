f1b7a036d9bc3214ee3bded70b9b61b50ce68bb6
Noah Flint <noah.flint24@gmail.com>
2019-06-27 10:50:22 -0600

    created most_revenue method for merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,8 +5,16 @@ class Merchant < ApplicationRecord
 
   
   def self.random
-    # require 'pry'; binding.pry
     random_id = all.pluck(:id).sample
     Merchant.find(random_id)
+  end
+
+  def self.most_revenue(limit = 1)
+    Merchant.joins('INNER JOIN items ON items.merchant_id = merchants.id INNER JOIN invoice_items ON invoice_items.item_id = items.id INNER JOIN invoices ON invoices.id = invoice_items.invoice_id INNER JOIN transactions ON transactions.invoice_id = invoices.id')
+        .where('transactions.result = ?', 'success')
+        .group(:id)
+        .select('SUM(invoice_items.quantity * invoice_items.unit_price) AS total_revenue, merchants.*')
+        .order('total_revenue DESC')
+        .limit(limit)
   end
 end
