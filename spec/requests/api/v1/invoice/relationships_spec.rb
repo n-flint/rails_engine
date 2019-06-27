c4aca5e4e8c3511ed24df2deddfa7ff67e43c149
Noah Flint <noah.flint24@gmail.com>
2019-06-27 01:38:49 -0600

    created invoices items relationship spec

--- spec/requests/api/v1/invoice/relationships_spec.rb
+++ spec/requests/api/v1/invoice/relationships_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe 'Invoices API' do
 
       @invoice_item1 = InvoiceItem.create(invoice: @invoice1, item: @item1, quantity: 1, unit_price: 100)
       @invoice_item2 = InvoiceItem.create(invoice: @invoice2, item: @item2, quantity: 2, unit_price: 200)
-      @invoice_item3 = InvoiceItem.create(invoice: @invoice2, item: @item2, quantity: 3, unit_price: 300)
+      @invoice_item3 = InvoiceItem.create(invoice: @invoice2, item: @item3, quantity: 3, unit_price: 300)
 
       @transaction1 = Transaction.create(invoice: @invoice1, credit_card_number: '12341234123412334', credit_card_expiration_date: '2019-05-12', result: 'success')
       @transaction2 = Transaction.create(invoice: @invoice1, credit_card_number: '00000000000000000', credit_card_expiration_date: '2019-04-11', result: 'success')
@@ -43,22 +43,42 @@ RSpec.describe 'Invoices API' do
       get "/api/v1/invoices/#{@invoice2.id}/invoice_items"
 
       data = JSON.parse(response.body)
-      # require 'pry'; binding.pry
 
       expect(data['data'][0]['attributes']['id'].to_i).to eq(@invoice_item2.id)
       expect(data['data'][0]['attributes']['quantity'].to_i).to eq(@invoice_item2.quantity)
       expect(data['data'][0]['attributes']['unit_price'].to_i).to eq(@invoice_item2.unit_price)
     
-
       expect(data['data'][1]['attributes']['id'].to_i).to eq(@invoice_item3.id)
       expect(data['data'][1]['attributes']['quantity'].to_i).to eq(@invoice_item3.quantity)
       expect(data['data'][1]['attributes']['unit_price'].to_i).to eq(@invoice_item3.unit_price)
-  
+    end
+
+    it 'can return an invoices items' do
+      get "/api/v1/invoices/#{@invoice2.id}/items"
+
+      data = JSON.parse(response.body)
+
+      expect(data['data'][0]['attributes']['id']).to eq(@item2.id)
+      expect(data['data'][0]['attributes']['name']).to eq(@item2.name)
+      expect(data['data'][0]['attributes']['description']).to eq(@item2.description)
+      expect(data['data'][0]['attributes']['unit_price']).to eq(@item2.unit_price)
+
+      expect(data['data'][1]['attributes']['id']).to eq(@item3.id)
+      expect(data['data'][1]['attributes']['name']).to eq(@item3.name)
+      expect(data['data'][1]['attributes']['description']).to eq(@item3.description)
+      expect(data['data'][1]['attributes']['unit_price']).to eq(@item3.unit_price)
+    end
+
+    it 'can return an invoices customer' do
+      get "/api/v1/invoices/#{@invoice3.id}/customer"
+
+      data = JSON.parse(response.body)
+
+      expect(data['data']['attributes']['id']).to eq(@customer1.id)
+      expect(data['data']['attributes']['first_name']).to eq(@customer1.first_name)
+      expect(data['data']['attributes']['last_name']).to eq(@customer1.last_name)
     end
   end
 end
-
-# GET /api/v1/invoices/:id/invoice_items returns a collection of associated invoice items
-# GET /api/v1/invoices/:id/items returns a collection of associated items
 # GET /api/v1/invoices/:id/customer returns the associated customer
 # GET /api/v1/invoices/:id/merchant returns the associated merchant
