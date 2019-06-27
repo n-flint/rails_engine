a2cffd34bf2ee75d8bd79a3d9fe4add458477042
Noah Flint <noah.flint24@gmail.com>
2019-06-26 23:09:56 -0600

    created item class method .random

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,9 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
   belongs_to :merchant
+
+  def self.random
+    random_id = pluck(:id).sample
+    Item.find(random_id)
+  end
 end
