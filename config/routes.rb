2b834c39fa94b2c1ce93c4c531bc898c050488fd
Noah Flint <noah.flint24@gmail.com>
2019-06-26 20:39:39 -0600

    added route to most_revenu#index

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
         get '/random', to: 'random#show'
+        get '/most_revenue', to: 'most_revenue#index'
         get '/:id/items', to: 'items#index'
         get '/:id/invoices', to: 'invoices#index'
       end
