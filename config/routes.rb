4733ffc30acc2150560252c27994736d18967371
Noah Flint <noah.flint24@gmail.com>
2019-06-27 00:44:19 -0600

    created route to invoice_items/item#show

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ Rails.application.routes.draw do
 
       namespace :invoice_items do
         get '/:id/invoice', to: 'invoice#show'
+        get '/:id/item', to: 'item#show'
       end
     end
   end
