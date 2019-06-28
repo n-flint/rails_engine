8ab6ea98473456f73da1ad6a70f7194533c383bd
Noah Flint <noah.flint24@gmail.com>
2019-06-27 18:32:23 -0600

    created favorite merchant method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,11 @@ class Customer < ApplicationRecord
                         :last_name
   has_many :invoices
   has_many :transactions, through: :invoices
+
+  def favorite_merchant
+    # returns merchants in incorrect order, works in rails c
+    id = self.id.to_s
+
+    Merchant.joins(items: {invoice_items: {invoice: :transactions}}).where('transactions.result = ?', 'success').where('invoices.customer_id =?', id).select('COUNT(merchants.id) AS merchant_count, merchants.*').group(:id).order('merchant_count DESC').limit(1)[0]
+  end
 end
