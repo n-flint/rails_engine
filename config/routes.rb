f5c17c37ac8d557ed5b8413b7313538402ef93ea
Noah Flint <noah.flint24@gmail.com>
2019-06-28 21:06:32 -0600

    created items/most_revenue route

--- config/routes.rb
+++ config/routes.rb
@@ -37,6 +37,7 @@ Rails.application.routes.draw do
         get '/find', to: 'search#show'
         get '/random', to: 'random#show'
         get '/:id/invoice_items', to: 'invoice_items#index'
+        get '/most_revenue', to: 'most_revenue#index'
         get '/:id/merchant', to: 'merchant#show'
       end
       resources :items, only: [:show]
