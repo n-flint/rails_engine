01615a976b0bd89792e5e809e68bd2981cee447c
Noah Flint <noah.flint24@gmail.com>
2019-06-28 20:05:09 -0600

    added more data to test setup

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Customer, type: :model do
   end
 
   describe 'instance methods' do
-    xit '#favorite_merchant' do
+    it '#favorite_merchant' do
     @customer1 = Customer.create(first_name: 'Bill', last_name: 'Burr')
     @merchant1 = Merchant.create(name: 'Afroman') 
     @merchant2 = Merchant.create(name: 'Superman') 
@@ -22,23 +22,27 @@ RSpec.describe Customer, type: :model do
     @invoice2 = Invoice.create(status: 'shipped', customer: @customer1, merchant: @merchant1)
     @invoice3 = Invoice.create(status: 'shipped', customer: @customer1, merchant: @merchant2)
     @invoice4 = Invoice.create(status: 'shipped', customer: @customer1, merchant: @merchant2)
+    @invoice5 = Invoice.create(status: 'shipped', customer: @customer1, merchant: @merchant1)
 
     @item1 = Item.create(name: 'item 1', description: 'item 1 description', unit_price: 100, merchant: @merchant1)
     @item2 = Item.create(name: 'item 2', description: 'item 2 description', unit_price: 100, merchant: @merchant1)
     @item3 = Item.create(name: 'item 3', description: 'item 3 description', unit_price: 200, merchant: @merchant2)
     @item4 = Item.create(name: 'item 4', description: 'item 4 description', unit_price: 200, merchant: @merchant2)
+    @item5 = Item.create(name: 'item 5', description: 'item 5 description', unit_price: 200, merchant: @merchant1)
 
     @invoice_item1 = InvoiceItem.create(invoice: @invoice1, item: @item1, quantity: 10, unit_price: 100)
     @invoice_item2 = InvoiceItem.create(invoice: @invoice2, item: @item2, quantity: 20, unit_price: 200)
     @invoice_item3 = InvoiceItem.create(invoice: @invoice3, item: @item3, quantity: 30, unit_price: 300)
     @invoice_item4 = InvoiceItem.create(invoice: @invoice4, item: @item4, quantity: 40, unit_price: 400)
+    @invoice_item5 = InvoiceItem.create(invoice: @invoice5, item: @item5, quantity: 40, unit_price: 400)
 
     @transaction1 = Transaction.create(invoice: @invoice1, credit_card_number: '12341234123412334', credit_card_expiration_date: '2019-05-12', result: 'success')
     @transaction2 = Transaction.create(invoice: @invoice2, credit_card_number: '00000000000000000', credit_card_expiration_date: '2019-04-11', result: 'success')
     @transaction3 = Transaction.create(invoice: @invoice3, credit_card_number: '00000000000000000', credit_card_expiration_date: '2019-04-11', result: 'success')
     @transaction4 = Transaction.create(invoice: @invoice4, credit_card_number: '00000000000000000', credit_card_expiration_date: '2019-04-11', result: 'success', created_at: '2019-03-11 14:53:59 UTC')
+    @transaction5 = Transaction.create(invoice: @invoice5, credit_card_number: '00000000000000000', credit_card_expiration_date: '2019-04-11', result: 'success', created_at: '2019-03-11 14:53:59 UTC')
     
-    expect(@customer1.favorite_merchant).to eq(@merchant2)
+    expect(@customer1.favorite_merchant).to eq(@merchant1)
     end
   end
 end
--- spec/requests/api/v1/customer/business_intel_spec.rb
+++ spec/requests/api/v1/customer/business_intel_spec.rb
@@ -10,31 +10,35 @@ RSpec.describe 'Customer Business Intelligence', type: :request do
     @invoice2 = Invoice.create(status: 'shipped', customer: @customer1, merchant: @merchant1)
     @invoice3 = Invoice.create(status: 'shipped', customer: @customer1, merchant: @merchant2)
     @invoice4 = Invoice.create(status: 'shipped', customer: @customer1, merchant: @merchant2)
+    @invoice5 = Invoice.create(status: 'shipped', customer: @customer1, merchant: @merchant1)
 
     @item1 = Item.create(name: 'item 1', description: 'item 1 description', unit_price: 100, merchant: @merchant1)
     @item2 = Item.create(name: 'item 2', description: 'item 2 description', unit_price: 100, merchant: @merchant1)
     @item3 = Item.create(name: 'item 3', description: 'item 3 description', unit_price: 200, merchant: @merchant2)
     @item4 = Item.create(name: 'item 4', description: 'item 4 description', unit_price: 200, merchant: @merchant2)
+    @item5 = Item.create(name: 'item 5', description: 'item 5 description', unit_price: 200, merchant: @merchant1)
 
     @invoice_item1 = InvoiceItem.create(invoice: @invoice1, item: @item1, quantity: 10, unit_price: 100)
     @invoice_item2 = InvoiceItem.create(invoice: @invoice2, item: @item2, quantity: 20, unit_price: 200)
     @invoice_item3 = InvoiceItem.create(invoice: @invoice3, item: @item3, quantity: 30, unit_price: 300)
     @invoice_item4 = InvoiceItem.create(invoice: @invoice4, item: @item4, quantity: 40, unit_price: 400)
+    @invoice_item5 = InvoiceItem.create(invoice: @invoice5, item: @item5, quantity: 40, unit_price: 400)
 
     @transaction1 = Transaction.create(invoice: @invoice1, credit_card_number: '12341234123412334', credit_card_expiration_date: '2019-05-12', result: 'success')
     @transaction2 = Transaction.create(invoice: @invoice2, credit_card_number: '00000000000000000', credit_card_expiration_date: '2019-04-11', result: 'success')
     @transaction3 = Transaction.create(invoice: @invoice3, credit_card_number: '00000000000000000', credit_card_expiration_date: '2019-04-11', result: 'success')
-    @transaction4 = Transaction.create(invoice: @invoice4, credit_card_number: '00000000000000000', credit_card_expiration_date: '2019-04-11', result: 'success', created_at: '2019-03-11 14:53:59 UTC')  
+    @transaction4 = Transaction.create(invoice: @invoice4, credit_card_number: '00000000000000000', credit_card_expiration_date: '2019-04-11', result: 'success', created_at: '2019-03-11 14:53:59 UTC')
+    @transaction5 = Transaction.create(invoice: @invoice5, credit_card_number: '00000000000000000', credit_card_expiration_date: '2019-04-11', result: 'success', created_at: '2019-03-11 14:53:59 UTC') 
   end
 
-  xit 'returns a customers favorit merchant' do
+  it 'returns a customers favorit merchant' do
     get "/api/v1/customers/#{@customer1.id}/favorite_merchant"
 
     expect(response).to be_successful
 
     data = JSON.parse(response.body)
     require 'pry'; binding.pry
-    expect(data['data']['attributes']['id']).to eq(@merchant2.id)
-    expect(data['data']['attributes']['name']).to eq(@merchant2.name)
+    expect(data['data']['attributes']['id']).to eq(@merchant1.id)
+    expect(data['data']['attributes']['name']).to eq(@merchant1.name)
   end
 end
