bbd4b2e57c4c5cfebf10acdca9d72d2126faf3da
Noah Flint <noah.flint24@gmail.com>
2019-06-30 12:40:05 -0600

    added created_at, updated_at to strong params

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -5,12 +5,13 @@ class Api::V1::Items::SearchController < ApplicationController
   end
 
   def index
+    # require 'pry'; binding.pry
     render json: ItemSerializer.new(Item.where(item_params))
   end
 
 
   private
     def item_params
-      params.permit(:id, :name, :description, :unit_price, :merchant_id)
+      params.permit(:id, :name, :description, :unit_price, :merchant_id, :created_at, :updated_at)
     end
 end
