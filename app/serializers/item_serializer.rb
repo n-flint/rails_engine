0be20dc59ad2760f2ad34ddf72d445670b4a8c7c
Noah Flint <noah.flint24@gmail.com>
2019-06-30 12:40:49 -0600

    added created_at, updated_at to atttributes

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,10 +1,10 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-    attributes :id, :name, :description, :merchant_id
+    attributes :id, :name, :description, :merchant_id, :created_at, :updated_at
 
     attribute :unit_price do |object|
       (object.unit_price.to_f / 100).to_s
     end
-
+    
     belongs_to :merchant
 end
