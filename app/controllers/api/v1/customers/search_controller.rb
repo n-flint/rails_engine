6e5d594f4418630a02ee83b0982c884877db52cf
Noah Flint <noah.flint24@gmail.com>
2019-06-30 10:59:03 -0600

    changed .where to .find_by in the show action

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Customers::SearchController < ApplicationController
 
   def show
-    render json: CustomerSerializer.new(Customer.where(customer_params))
+    render json: CustomerSerializer.new(Customer.find_by(customer_params))
   end
 
   def index
